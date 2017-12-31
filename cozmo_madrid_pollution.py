9e785a4f10a972519af829a6ad04594624e1dbce
Julián Caro Linares <jcarolinares@gmail.com>
2017-12-31 13:55:50 +0100

    Scrapped and Cozmo interaction done
    First version done

--- cozmo_madrid_pollution.py
+++ cozmo_madrid_pollution.py
@@ -3,89 +3,102 @@ import urllib
 import requests
 import os
 import sys
+import time
 
 #Cozmo SDK
 import cozmo
+from cozmo.util import degrees, distance_mm, speed_mmps
 
-#Downloading page
-r  = requests.get('http://www.mambiente.munimadrid.es/opencms/opencms/calaire/consulta/Gases_y_particulas/informegaseshorarios_antiguo.html?__locale=es')
-data = r.text
-soup = BeautifulSoup(data,"lxml")
+#Global lists
+station_data=[]
+stations_list=[]
 
-#Scrapping the NO2 values
-date=soup.find('span',class_="tabla_titulo_hora")
-#date=str(date)
-#last_issue=last_issue[last_issue.find("MagPi")+5:last_issue.find(">")-5]
 
-date=date.strings
-#print(date)
+def scrapping_madrid_pollution():
 
-for data in date:
-    print(data+"\n")
+    #Downloading page
+    r  = requests.get('http://www.mambiente.munimadrid.es/opencms/opencms/calaire/consulta/Gases_y_particulas/informegaseshorarios_antiguo.html?__locale=es')
+    data = r.text
+    soup = BeautifulSoup(data,"lxml")
 
-stations_data=soup.find_all('td',class_="primertd")
+    #Scrapping the pollution values
+    date=soup.find('span',class_="tabla_titulo_hora")
+    #date=str(date)
+    #last_issue=last_issue[last_issue.find("MagPi")+5:last_issue.find(">")-5]
 
-for station in stations_data:
-    print(str(station.string));
-    station.headers
+    date=date.strings
+    #print(date)
+
+    for data in date:
+        print(data+"\n")
+
+    #stations_data=soup.find_all('td',class_="primertd")
+
+    stations_raw_data=soup.find_all('tr')
+
+    #Removing headers (twice) from the list
+    del stations_raw_data[0]
+    del stations_raw_data[0]
+
+
+    #Station data
+    #station_data={'STATION':'','PM10':'','PM2.5':'','SO2':'','CO':'','O3':'','NO2':'','BEN':'','TOL':''}
+
+    for station in stations_raw_data:
+        station_name=station.td.string
+        auxiliar=station.td.find_next_siblings()
+
+        #Station name
+        station_data.append(station_name)
+        #print(station_data[0])
+
+        #Stations values
+        for value in auxiliar:
+            if value.string=="-":
+                station_data.append(0)
+            else:
+                station_data.append(float(value.string))
+        
+        #print (station_data)
+        stations_list.append(station_data[:])
+        del station_data[:]    
+
+    #print(stations_list)
+
+    for station in stations_list:
+        print(station)
+
 def cozmo_program(robot: cozmo.robot.Robot):
 
-    print("COZMO:")
-
-#    for stations in stations_data:
-#        robot.say_text(stations.string).wait_for_completed()
-    #robot.play_anim(name="id_poked_giggle").wait_for_completed()
-    #cube1.set_lights(cozmo.lights.green_light)
-    #cube2.set_lights(cozmo.lights.blue_light)
-    #cube3.set_lights(cozmo.lights.red_light)
-    #robot.say_text("We wish you a happy new year!",in_parallel=True)#.wait_for_completed()
-    #robot.play_anim_trigger(cozmo.anim.Triggers.CodeLabWin, in_parallel=False).wait_for_completed()
-    #robot.say_text("We wish you a happy new year!",in_parallel=False,play_excited_animation=True).wait_for_completed()
+    print("COZMO MADRID POLLUTION")
+    print("\nDOWNLOADING POLLUTION DATA\n")
+    robot.turn_in_place(degrees(360))#.wait_for_completed()
 
 
+    #robot.drive_wheels(125, -1)
+    scrapping_madrid_pollution()
+    #robot.stop_all_motors()
 
-    #Animation lists
-    #coz = robot.wait_for_robot()
-    #print("All animations = %s" % coz.anim_names)
+    robot.say_text("The pollution in Madrid in the last hour is: ").wait_for_completed()
 
-    
-    #CodeLabPartyTime
-
-    # Keep the lights on for 10 seconds until the program exits
-    #time.sleep(10)
+    #for station in stations_list:
+    #    robot.say_text("At "+station[0]).wait_for_completed()
+    #    robot.say_text("The NO2 level is "+str(station[6])).wait_for_completed()
 
 
+    #Demo mode
+    robot.say_text("At "+stations_list[0][0],play_excited_animation=True).wait_for_completed()
+    robot.say_text("The NO2 level is "+str(stations_list[0][6]),).wait_for_completed()
+
+    robot.say_text("At "+stations_list[6][0],play_excited_animation=True).wait_for_completed()
+    robot.say_text("The NO2 level is "+str(stations_list[6][6])).wait_for_completed()
+
+    robot.say_text("At "+stations_list[9][0],play_excited_animation=True).wait_for_completed()
+    robot.say_text("The NO2 level is "+str(stations_list[9][6])).wait_for_completed()
+
+
+    robot.say_text("Not pollution alerts declared").wait_for_completed()
     
 cozmo.run_program(cozmo_program)
 
 
-
-'''
-#Quotes
-#for actual_quote in web_body:
-for actual_quote in list_of_quotes:
-    auxiliar_object=quote()
-    auxiliar_object.new(key,actual_quote.get_text())
-    quotes_objects.append(auxiliar_object)
-    #auxiliar_object.show_quote()
-
-'''
-
-#http://www.mambiente.munimadrid.es/opencms/opencms/calaire/consulta/Gases_y_particulas/informegaseshorarios.html?__locale=es
-
-
-
-'''
-print("\nLast issue number: {}".format(last_issue))
-
-for issue in range(int(last_issue)):
-    try:
-        issue=issue+1
-        if not os.path.exists("./magpi-issues/Magpi{:02d}.pdf".format(issue)):
-            print("Downloading https://www.raspberrypi.org/magpi-issues/MagPi{:02d}.pdf".format(issue))
-            urllib.urlretrieve ("https://www.raspberrypi.org/magpi-issues/MagPi{:2d}.pdf".format(issue), "./magpi-issues/Magpi{:02d}.pdf".format(issue))
-    except():
-        print(" \nERROR: There was an error downloading Magpi{:02d}\n".format(issue))
-    else:
-        print(" MagPi{:02d} Downloaded!\n".format(issue))
-'''
